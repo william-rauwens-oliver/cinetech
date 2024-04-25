248c9a47303c6190818720a38e6d350a95929387
Justine Saint-Martin <justine.saint-martin@laplateforme.io>
2024-04-25 10:35:01 +0200

    ( ajout note avec etoiles en reliant a l'api)

--- js/affichage.js
+++ js/affichage.js
@@ -9,26 +9,38 @@ const requests = {
   fetchNetflixOrignals: `${base_url}/discover/tv?${api}&with_networks=213&language=fr-FR`,
 };
 
-
 function truncate(str, n) {
   return str?.length > n ? str.substr(0, n - 1) + "..." : str;
 }
+
+function getStars(vote) {
+  const starTotal = 5;
+  const starPercentage = (vote / 10) * starTotal;
+  const starRounded = Math.round(starPercentage * 10) / 10;
+  let stars = "<span class='rating-stars'>";
+  for (let i = 0; i < starTotal; i++) {
+    stars += i < starRounded ? "★" : "☆";
+  }
+  stars += "</span>";
+  return stars;
+}
+
 
 fetch(requests.fetchNetflixOrignals)
 .then((res) => res.json())
 .then((data) => {
   console.log(data.results);
- 
+
   const setMovie = data.results[Math.floor(Math.random() * data.results.length - 1)];
   console.log(setMovie);
   var banner = document.getElementById("banner");
   var banner_title = document.getElementById("banner__title");
   var banner__desc = document.getElementById("banner__description");
-  var banner__rate = document.getElementById("banner__rate")
+  var banner__rate = document.getElementById("banner__rate");
   banner.style.backgroundImage = "url(" + img_url + setMovie.backdrop_path + ")";
   banner__desc.innerText = truncate(setMovie.overview, 150);
-  banner_title.innerText = setMovie.name;
-  banner__rate.innerText = setMovie.vote_average;
+  banner_title.innerText = setMovie.name || setMovie.title; // Adaptation pour TV ou films
+  banner__rate.innerHTML = getStars(setMovie.vote_average);
 })
 
 fetch(requests.fetchPopular)
@@ -53,7 +65,6 @@ fetch(requests.fetchPopular)
     poster.id = s2;
     poster.src = img_url + movie.poster_path;
     row_posters.appendChild(poster);
-
   });
 });
 
@@ -79,6 +90,5 @@ fetch(requests.fetchTrending)
     poster.id = s2;
     poster.src = img_url + movie.poster_path;
     row_posters.appendChild(poster);
-
   });
 });
